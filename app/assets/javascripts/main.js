617b69e7a1be2d6428699ed7ddb19e6d0b2f143f
mandyyeung <chimmyy@gmail.com>
2016-01-18 21:42:35 -0500

    Add dismissible alerts

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
   });
+  $(".alert-notice").fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert-notice").alert('close');
+  });
 });
