7a6de20f71e61dab71f5849f63500f59dae8a891
mandyyeung <chimmyy@gmail.com>
2016-01-24 20:41:12 -0500

    Remove search from header (for now). Add badges for statuses

--- app/assets/javascripts/pcos.js
+++ app/assets/javascripts/pcos.js
@@ -2,4 +2,25 @@ $(document).ready(function(){
   $('#pcos').DataTable({
     responsive: true
   });
+  $('.label').each(function(){
+    var $this = $(this),
+        classToAdd = null;
+    switch ($this.text()){
+      case 'Approved':
+        classToAdd = 'label-success';
+        break;
+      case 'Pending':
+        classToAdd = 'label-primary';
+        break;
+      case 'Approximate':
+        classToAdd = 'label-danger';
+        break;
+      case 'Void':
+        classToAdd = 'label-default';
+        break;
+    }
+    if (classToAdd !== null){
+      $this.addClass(classToAdd);
+    }
+  });
 });
